1cb30f46a47ef0932830c096718b8b3e89a29a22
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-16 08:31:20 +1000

    Add files via upload

---
+++ Services/service.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+function freshafunc() {
+    window.open("https://www.fresha.com/a/renzoblendz-springfield-springfield-parkway-j5ra8m9u?pId=1480652")
+}
