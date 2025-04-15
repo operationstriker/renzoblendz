fe4ec3e04b94ee1eb73f9439349e31bf582c8a73
Robert Orule <159502197+operationstriker@users.noreply.github.com>
2025-04-16 08:22:43 +1000

    Update service.js

--- Services/service.js
+++ Services/service.js
@@ -5,4 +5,7 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
+function freshafunc() {
+    window.open("https://www.fresha.com/a/renzoblendz-springfield-springfield-parkway-j5ra8m9u?pId=1480652")
+}
 
